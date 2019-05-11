768c22c4feebf45f10e21cb6131957f3e0deb430
heman <heman.1682@gmail.com>
2019-05-12 02:16:18 +0430

    initial models

--- masters/envs/production.py
+++ masters/envs/production.py
@@ -1,1 +1,13 @@
 from .common import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['devmasters.ir']
+
+STATICFILES_DIRS = [
+    PROJECT_DIR + '/masters/statics'
+]
+
+STATIC_URL = '/static/'
+STATIC_ROOT = "/avat/public/devmaster.ir/static/"
+
+
